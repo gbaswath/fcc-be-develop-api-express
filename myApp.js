e24270de172772f1af0bde9f598630e49f9d4878
G B Aswath <gbaswath@zohomail.in>
2022-02-22 15:51:43 +0530

    Use .env property configuration
    1. myApp.js -> Use process.env.property to check for case sensitive configuration. Accordingly return corresponding json
    
    2. package.json, package-lock.json -> Added dotenv package to use .env config

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 module.exports = app;
 console.log("Hello World");
 var absolutePath = __dirname;
@@ -9,7 +10,13 @@ app.get("/", function (req, res) {
    res.sendFile(absolutePath + "/views/index.html");
 });
 app.get("/json", function (req, res) {
-   var data = {"message": "Hello json"};
+   var lowerCaseData = {"message": "Hello json"};
+   var upperCaseData = {"message": "HELLO JSON"};
+   var data;
+   if(process.env.MESSAGE_STYLE === 'uppercase')
+      data = upperCaseData;
+   else 
+      data = lowerCaseData;
    console.log("Data " + JSON.stringify(data));
    res.json(data);
 });
