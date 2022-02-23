e1f469515c1e0a36770fe9c5830fc4ab7c17e22f
G B Aswath <gbaswath@zohomail.in>
2022-02-23 16:48:28 +0530

    Root Level Logger Middleware
    1. myApp.js -> app.use to set middleware function to intercept all requests and print console logger.

--- myApp.js
+++ myApp.js
@@ -6,16 +6,20 @@ console.log("Hello World");
 var absolutePath = __dirname;
 console.log("Path " + absolutePath);
 app.use("/public", express.static(absolutePath + "/public"));
+app.use(function (req, res, next) {
+   console.log(req.method + " " + req.path + " - " + req.ip);
+   next();
+});
 app.get("/", function (req, res) {
    res.sendFile(absolutePath + "/views/index.html");
 });
 app.get("/json", function (req, res) {
-   var lowerCaseData = {"message": "Hello json"};
-   var upperCaseData = {"message": "HELLO JSON"};
+   var lowerCaseData = { "message": "Hello json" };
+   var upperCaseData = { "message": "HELLO JSON" };
    var data;
-   if(process.env.MESSAGE_STYLE === 'uppercase')
+   if (process.env.MESSAGE_STYLE === 'uppercase')
       data = upperCaseData;
-   else 
+   else
       data = lowerCaseData;
    console.log("Data " + JSON.stringify(data));
    res.json(data);
