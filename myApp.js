2d1f11c5f23829532088981d565a6e5251126619
G B Aswath <gbaswath@zohomail.in>
2022-02-24 15:28:12 +0530

    Mounted Middleware for Form Encoding
    1. myApp.js -> Included Comments & Mounted Request Body Parser

--- myApp.js
+++ myApp.js
@@ -1,18 +1,28 @@
+//Required Imports
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 require('dotenv').config();
 module.exports = app;
+//Startup Log
 console.log("Hello World");
-var absolutePath = __dirname;
+//Constant to Absolute Path Reference
+const absolutePath = __dirname;
 console.log("Path " + absolutePath);
+//Use & Load Static Asset
 app.use("/public", express.static(absolutePath + "/public"));
+//Use Form Encoding of Post Data
+app.use(bodyParser.urlencoded({ extended: false }));
+//Middleware to Log Request Details
 app.use(function (req, res, next) {
    console.log(req.method + " " + req.path + " - " + req.ip);
    next();
 });
+//Default Landing Page
 app.get("/", function (req, res) {
    res.sendFile(absolutePath + "/views/index.html");
 });
+//Return JSON Response
 app.get("/json", function (req, res) {
    var lowerCaseData = { "message": "Hello json" };
    var upperCaseData = { "message": "HELLO JSON" };
@@ -24,6 +34,7 @@ app.get("/json", function (req, res) {
    console.log("Data " + JSON.stringify(data));
    res.json(data);
 });
+//Return Current Time as JSON Data
 app.get("/now", function (req, res, next) {
    req.time = new Date().toString();
    next();
@@ -32,6 +43,7 @@ app.get("/now", function (req, res, next) {
    console.log("Time Data " + JSON.stringify(timeData));
    res.send(timeData);
 });
+//Print Query Params Reference
 app.get("/:word/echo", function (req, res) {
    var echoData = req.params.word;
    echoData = { "echo": echoData };
